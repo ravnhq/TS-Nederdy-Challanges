db715f225c7555ea1a5a1f80f11f144351b93b10
Irvin Sanchez <irvnsanchez@gmail.com>
2021-04-29 11:42:38 -0600

    update test and ts file

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+// example interfaces that can be use
+// TIP: the types mentioned in the interfaces must be fulfilled in order to solve the problem.
 interface TemperatureReading {
   time: Date
   temperature: number
@@ -10,74 +12,16 @@ interface TemperatureSummary {
   low: number
   average: number
 }
-type DaysType =
-  | "sunday"
-  | "monday"
-  | "tuesday"
-  | "wednesday"
-  | "thursday"
-  | "friday"
-  | "saturday"
-type CitySummaryType = Record<
-  string,
-  Partial<Record<DaysType, TemperatureSummary>>
->
-type CitySummaryCollectionType = Record<
-  string,
-  Partial<Record<DaysType, number[]>>
->
-const numberToDays: Record<number, DaysType> = {
-  0: "sunday",
-  1: "monday",
-  2: "tuesday",
-  3: "wednesday",
-  4: "thursday",
-  5: "friday",
-  6: "saturday",
-}
-const database: CitySummaryType = {}
-const example: TemperatureReading[] = [
-  {
-    time: new Date("1/1/2021"),
-    temperature: 10,
-    city: "Utah",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 9,
-    city: "Utah",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 11,
-    city: "Utah",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 3,
-    city: "New York",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 2,
-    city: "New York",
-  },
-  {
-    time: new Date("1/1/2021"),
-    temperature: 7,
-    city: "New York",
-  },
-]
+
 function processReadings(readings: TemperatureReading[]): void {
   // add here your code
 }
 function getTemperatureSummary(
   date: Date,
   city: string,
-): TemperatureSummary | null | void {
+): void {
   //add here your code
 }
 
 exports.processReadings = processReadings
 exports.getTemperatureSummary = getTemperatureSummary
-exports.example = example
--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,42 @@
 const {
   processReadings,
   getTemperatureSummary,
-  example,
 } = require("../dist/app")
+
 const dateTest = new Date("1/1/2021")
+
+const example = [
+  {
+    time: new Date("1/1/2021"),
+    temperature: 10,
+    city: "Utah",
+  },
+  {
+    time: new Date("1/1/2021"),
+    temperature: 9,
+    city: "Utah",
+  },
+  {
+    time: new Date("1/1/2021"),
+    temperature: 11,
+    city: "Utah",
+  },
+  {
+    time: new Date("1/1/2021"),
+    temperature: 3,
+    city: "New York",
+  },
+  {
+    time: new Date("1/1/2021"),
+    temperature: 2,
+    city: "New York",
+  },
+  {
+    time: new Date("1/1/2021"),
+    temperature: 7,
+    city: "New York",
+  },
+]
 
 describe("Process information", () => {
   test("run function with the values", () => {
