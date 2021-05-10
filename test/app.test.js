74d9a64706b2d6fe0dd1857eef466fd3edd654a3
Fernando Juarez <fernando@ravn.co>
2021-05-09 23:43:18 -0600

    update challenge

--- test/app.test.js
+++ test/app.test.js
@@ -1,85 +1,82 @@
-const {
-	processReadings,
-	getTemperatureSummary,
-} = require('../dist/app');
+const { processReadings, getTemperatureSummary } = require('../dist/app')
 
 const example = [
-	{
-		time: new Date('1/3/2021'),
-		temperature: 8,
-		city: 'Utah',
-	},
-	{
-		time: new Date('1/2/2021'),
-		temperature: 10,
-		city: 'Utah',
-	},
-	{
-		time: new Date('1/2/2021'),
-		temperature: 9,
-		city: 'Utah',
-	},
-	{
-		time: new Date('1/2/2021'),
-		temperature: 12,
-		city: 'Utah',
-	},
-	{
-		time: new Date('1/2/2021'),
-		temperature: 11,
-		city: 'Utah',
-	},
-	{
-		time: new Date('3/12/2021'),
-		temperature: 15,
-		city: 'New York',
-	},
-	{
-		time: new Date('3/12/2021'),
-		temperature: 10,
-		city: 'New York',
-	},
-	{
-		time: new Date('3/12/2021'),
-		temperature: 11,
-		city: 'New York',
-	},
-	{
-		time: new Date('3/12/2021'),
-		temperature: 9,
-		city: 'New York',
-	},
-	{
-		time: new Date('3/13/2021'),
-		temperature: 16,
-		city: 'New York',
-	},
-];
+  {
+    time: new Date('1/3/2021'),
+    temperature: 8,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 10,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 9,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 12,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 11,
+    city: 'Utah',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 15,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 10,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 11,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 9,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/13/2021'),
+    temperature: 16,
+    city: 'New York',
+  },
+]
 
 describe('Process information', () => {
-	test('run function with the values', () => {
-		expect(processReadings(example));
-	});
-});
+  test('run function with the values', () => {
+    expect(processReadings(example))
+  })
+})
 
 describe('Temperature Summary', () => {
-	test('run function getTemperatureSummary for Utah', () => {
-		const result = getTemperatureSummary(new Date('1/2/2021'), 'Utah');
+  test('run function getTemperatureSummary for Utah', () => {
+    const result = getTemperatureSummary(new Date('1/2/2021'), 'Utah')
 
-		expect(result).toHaveProperty('first', 10);
-		expect(result).toHaveProperty('last', 11);
-		expect(result).toHaveProperty('high', 12);
-		expect(result).toHaveProperty('low', 9);
-		expect(result).toHaveProperty('average', 10.5);
-	});
+    expect(result).toHaveProperty('first', 10)
+    expect(result).toHaveProperty('last', 11)
+    expect(result).toHaveProperty('high', 12)
+    expect(result).toHaveProperty('low', 9)
+    expect(result).toHaveProperty('average', 10.5)
+  })
 
-	test('run function getTemperatureSummary for New York', () => {
-		const result = getTemperatureSummary(new Date('3/12/2021'), 'New York');
+  test('run function getTemperatureSummary for New York', () => {
+    const result = getTemperatureSummary(new Date('3/12/2021'), 'New York')
 
-		expect(result).toHaveProperty('first', 15);
-		expect(result).toHaveProperty('last', 9);
-		expect(result).toHaveProperty('high', 15);
-		expect(result).toHaveProperty('low', 9);
-		expect(result).toHaveProperty('average', 11.25);
-	});
-});
+    expect(result).toHaveProperty('first', 15)
+    expect(result).toHaveProperty('last', 9)
+    expect(result).toHaveProperty('high', 15)
+    expect(result).toHaveProperty('low', 9)
+    expect(result).toHaveProperty('average', 11.25)
+  })
+})
