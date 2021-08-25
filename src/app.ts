d7ab003149bc17f1e3f6b1cfb3cc3f58e8898d8a
Patrick <patrickdz96@gmail.com>
2021-08-25 12:57:22 -0500

    add temperature summary

--- src/app.ts
+++ src/app.ts
@@ -13,16 +13,38 @@ interface TemperatureSummary {
   average: number
 }
 
-const data: Array<TemperatureReading> = []
+const temperatures: Array<TemperatureReading> = []
 
 export function processReadings(readings: TemperatureReading[]): void {
-  data.push(...readings);
+  temperatures.push(...readings);
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const filteredTemperatures = temperatures.filter(value => value.time.getTime() === new Date(date).getTime() && value.city === city);
+  const totalFilteredTemperatures = filteredTemperatures.length;
+
+  if (totalFilteredTemperatures > 0) {
+    const summary: TemperatureSummary = {
+      first: filteredTemperatures[0].temperature,
+      last: filteredTemperatures[totalFilteredTemperatures - 1].temperature,
+      high: filteredTemperatures[0].temperature,
+      low: filteredTemperatures[0].temperature,
+      average: filteredTemperatures[0].temperature,
+    };
+
+    for (let i = 1; i < filteredTemperatures.length; i++) {
+      summary.high = summary.high < filteredTemperatures[i].temperature ? filteredTemperatures[i].temperature : summary.high;
+      summary.low = summary.low > filteredTemperatures[i].temperature ? filteredTemperatures[i].temperature : summary.low;
+      summary.average += filteredTemperatures[i].temperature;
+    }
+
+    summary.average = Math.round((summary.average / totalFilteredTemperatures) * 100) / 100;
+
+    return summary;
+  }
+
+  return null;
 }
