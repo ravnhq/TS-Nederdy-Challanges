403b763c71bb53eaee314d4c925555844a0ad3e5
Kevin Cotrina <kcotrinam92@gmail.com>
2021-08-25 23:55:44 -0500

    Add solution for processReadings and getTemperatureSummary

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ interface TemperatureReading {
   temperature: number
   city: string
 }
+
 interface TemperatureSummary {
   first: number
   last: number
@@ -13,14 +14,60 @@ interface TemperatureSummary {
   average: number
 }
 
+const temperatures: TemperatureReading[] = []
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  temperatures.push(...readings)
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const temperatureSummary: TemperatureSummary = {
+    first: 0,
+    last: 0,
+    high: 0,
+    low: 0,
+    average: 0,
+  }
+
+  const filteredTemperatures = temperatures.filter(
+    (temp) =>
+      temp.time.getTime() === new Date(date).getTime() && temp.city === city,
+  )
+  const filteredTempLegnth: number = filteredTemperatures.length
+
+  if (filteredTempLegnth > 0) {
+    temperatureSummary.first = filteredTemperatures[0].temperature
+    temperatureSummary.last =
+      filteredTemperatures[filteredTempLegnth - 1].temperature
+  }
+
+  filteredTemperatures.forEach((temperature, index) => {
+    temperatureSummary.high = Math.max(
+      temperature.temperature,
+      temperatureSummary.high,
+    )
+
+    if (index === 0) {
+      temperatureSummary.low = temperature.temperature
+    } else {
+      temperatureSummary.low = Math.min(
+        temperatureSummary.low,
+        temperature.temperature,
+      )
+    }
+
+    if (index < filteredTempLegnth - 1) {
+      temperatureSummary.average =
+        temperatureSummary.average + temperature.temperature
+    } else {
+      temperatureSummary.average =
+        (temperatureSummary.average + temperature.temperature) /
+        filteredTempLegnth
+    }
+  })
+
+  return filteredTempLegnth ? temperatureSummary : null
 }
