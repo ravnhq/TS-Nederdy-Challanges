a8ea4e6399e64baf85f47c9aa524ccf5440e6031
Abel AR <abel_john_14@hotmail.com>
2021-10-26 17:30:36 -0500

    Filter data by day and time

---
+++ src/app.js
@@ -0,0 +1,109 @@
+"use strict";
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+exports.__esModule = true;
+exports.getTemperatureSummary = exports.processReadings = void 0;
+function processReadings(readings) {
+    // add here your code
+    var getDates = readings.map(function (_a) {
+        var time = _a.time;
+        return time.toDateString();
+    });
+    var filterRepeatDates = __spreadArray([], __read(new Set(getDates)));
+    var divideArrayByDate = filterRepeatDates.map(function (dateTime) {
+        return readings.filter(function (temperatureReading) {
+            if (temperatureReading.time.toDateString() === dateTime) {
+                return temperatureReading;
+            }
+        });
+    });
+    var newArray = divideArrayByDate.map(function (temperatureReading) {
+        if (temperatureReading.length <= 1) {
+            return temperatureReading.find(function (a) { return a; });
+        }
+        else {
+            return temperatureReading;
+        }
+    });
+    console.log(newArray);
+    // })
+}
+exports.processReadings = processReadings;
+function getTemperatureSummary(date, city) {
+    //add here your code
+    return null;
+}
+exports.getTemperatureSummary = getTemperatureSummary;
+var example = [
+    {
+        time: new Date('1/3/2021'),
+        temperature: 8,
+        city: 'Utah'
+    },
+    {
+        time: new Date('1/2/2021'),
+        temperature: 10,
+        city: 'Utah'
+    },
+    {
+        time: new Date('1/2/2021'),
+        temperature: 9,
+        city: 'Utah'
+    },
+    {
+        time: new Date('1/2/2021'),
+        temperature: 12,
+        city: 'Utah'
+    },
+    {
+        time: new Date('1/2/2021'),
+        temperature: 11,
+        city: 'Utah'
+    },
+    {
+        time: new Date('3/12/2021'),
+        temperature: 15,
+        city: 'New York'
+    },
+    {
+        time: new Date('3/12/2021'),
+        temperature: 10,
+        city: 'New York'
+    },
+    {
+        time: new Date('3/12/2021'),
+        temperature: 11,
+        city: 'New York'
+    },
+    {
+        time: new Date('3/12/2021'),
+        temperature: 9,
+        city: 'New York'
+    },
+    {
+        time: new Date('3/13/2021'),
+        temperature: 16,
+        city: 'New York'
+    },
+];
+processReadings(example);
+// console.log(processReadings(example))
--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,26 @@ interface TemperatureSummary {
 
 export function processReadings(readings: TemperatureReading[]): void {
   // add here your code
+  const getDates = readings.map(({time}) => time.toDateString());
+  const filterRepeatDates = [...new Set<string>(getDates)];
+  const divideArrayByDate = filterRepeatDates.map( dateTime => {
+    return readings.filter( temperatureReading => {
+      if(temperatureReading.time.toDateString() === dateTime) {
+        return temperatureReading;
+      }
+    })
+  });
+
+  const newArray = divideArrayByDate.map( temperatureReading => {
+    if(temperatureReading.length <= 1) {
+      return temperatureReading.find(a => a);
+    } else {
+      return temperatureReading;
+    }
+  })
+  console.log(newArray);
+    
+  // })
 }
 
 export function getTemperatureSummary(
@@ -24,3 +44,59 @@ export function getTemperatureSummary(
   //add here your code
   return null
 }
+
+const example = [
+  {
+    time: new Date('1/3/2021'),
+    temperature: 8,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 10,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 9,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 12,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 11,
+    city: 'Utah',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 15,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 10,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 11,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 9,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/13/2021'),
+    temperature: 16,
+    city: 'New York',
+  },
+];
+
+processReadings(example)
+// console.log(processReadings(example))
