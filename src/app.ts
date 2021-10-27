e97d5bf2fa6e1fc3cb8a4317b369d4b40ac5b695
Marcelo Valdivia <marceloevv08@gmail.com>
2021-10-27 14:46:46 -0500

    fix: Types validation and first function process

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // example interfaces that can be use
 // TIP: the types mentioned in the interfaces must be fulfilled in order to solve the problem.
 interface TemperatureReading {
@@ -13,61 +14,17 @@ interface TemperatureSummary {
   average: number
 }
 
-const example = [
-  {
-    time: new Date('1/3/2021'),
-    temperature: 8,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 10,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 9,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 12,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 11,
-    city: 'Utah',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 15,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 10,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 11,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 9,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/13/2021'),
-    temperature: 16,
-    city: 'New York',
-  },
-]
+const temperatures: TemperatureReading[] = []
 
 export function processReadings(readings: TemperatureReading[]): void {
-  console.log(readings)
+  for (const reading of readings) {
+    const readingInt: TemperatureReading = {
+      time: reading.time,
+      temperature: reading.temperature,
+      city: reading.city,
+    }
+    temperatures.push(readingInt)
+  }
   return
 }
 
@@ -75,7 +32,7 @@ export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  const groupReadings = example.reduce(
+  const groupReadings = temperatures.reduce(
     (tmp: TemperatureReading | any, obj: TemperatureReading) =>
       Object.assign(tmp, {
         [obj.city]: (tmp[obj.city] || []).concat(obj),
@@ -83,23 +40,23 @@ export function getTemperatureSummary(
     [],
   )
   const readFiltered = groupReadings[city]
-  if (groupReadings[city]) {
-    const datedReadings = readFiltered.filter(
-      (item: TemperatureReading) => item.time.toString() === date.toString(),
-    )
-    const temperaturesReading = datedReadings.map(
-      (item: TemperatureReading) => item.temperature,
-    )
-    const temperatureSummary: TemperatureSummary = {
-      first: temperaturesReading[0],
-      last: temperaturesReading[temperaturesReading.length - 1],
-      high: Math.max(...temperaturesReading),
-      low: Math.min(...temperaturesReading),
-      average:
-        temperaturesReading.reduce((a: number, b: number) => a + b, 0) /
-        temperaturesReading.length,
-    }
-    return temperatureSummary
+  if (!groupReadings[city]) {
+    return null
   }
-  return null
+  const datedReadings = readFiltered.filter(
+    (item: TemperatureReading) => item.time.toString() === date.toString(),
+  )
+  const temperaturesReading = datedReadings.map(
+    (item: TemperatureReading) => item.temperature,
+  )
+  const temperatureSummary: TemperatureSummary = {
+    first: temperaturesReading[0],
+    last: temperaturesReading[temperaturesReading.length - 1],
+    high: Math.max(...temperaturesReading),
+    low: Math.min(...temperaturesReading),
+    average:
+      temperaturesReading.reduce((a: number, b: number) => a + b, 0) /
+      temperaturesReading.length,
+  }
+  return temperatureSummary
 }
