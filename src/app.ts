ca1b6a92d23d0df8074d3fb658db155713a29ac0
Marcelo Valdivia <marceloevv08@gmail.com>
2021-10-27 13:48:24 -0500

    feat: getSummary function

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,93 @@ interface TemperatureSummary {
   average: number
 }
 
+const example = [
+  {
+    time: new Date('1/3/2021'),
+    temperature: 8,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 10,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 9,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 12,
+    city: 'Utah',
+  },
+  {
+    time: new Date('1/2/2021'),
+    temperature: 11,
+    city: 'Utah',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 15,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 10,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 11,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/12/2021'),
+    temperature: 9,
+    city: 'New York',
+  },
+  {
+    time: new Date('3/13/2021'),
+    temperature: 16,
+    city: 'New York',
+  },
+]
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  console.log(readings)
+  return
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
+  const groupReadings = example.reduce(
+    (tmp: TemperatureReading | any, obj: TemperatureReading) =>
+      Object.assign(tmp, {
+        [obj.city]: (tmp[obj.city] || []).concat(obj),
+      }),
+    [],
+  )
+  const readFiltered = groupReadings[city]
+  if (groupReadings[city]) {
+    const datedReadings = readFiltered.filter(
+      (item: TemperatureReading) => item.time.toString() === date.toString(),
+    )
+    const temperaturesReading = datedReadings.map(
+      (item: TemperatureReading) => item.temperature,
+    )
+    const temperatureSummary: TemperatureSummary = {
+      first: temperaturesReading[0],
+      last: temperaturesReading[temperaturesReading.length - 1],
+      high: Math.max(...temperaturesReading),
+      low: Math.min(...temperaturesReading),
+      average:
+        temperaturesReading.reduce((a: number, b: number) => a + b, 0) /
+        temperaturesReading.length,
+    }
+    return temperatureSummary
+  }
   return null
 }
