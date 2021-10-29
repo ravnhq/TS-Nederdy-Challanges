a3b8e312553954c319224f8feecb31dae8b8741e
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-10-28 22:18:27 -0500

    feat: implemented solution for the problem

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,62 @@ interface TemperatureSummary {
   average: number
 }
 
-export function processReadings(readings: TemperatureReading[]): void {
+export function processReadings(
+  readings: TemperatureReading[],
+): TemperatureReading[] {
   // add here your code
+  return readings
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
+  // needed to pass the temperature readings array to work with it
+  readings: TemperatureReading[],
 ): TemperatureSummary | null {
   //add here your code
-  return null
+
+  const processedWithForOf: TemperatureReading[] = []
+
+  for (const read of readings) {
+    if (read.city === city && read.time.getDate() === date.getDate()) {
+      processedWithForOf.push(read)
+    }
+  }
+
+  const processedWithArrMethod = readings.filter(
+    (read) => read.city === city && read.time.getDate() === date.getDate(),
+  )
+
+  const first = processedWithForOf[0]?.temperature
+
+  const high = processedWithArrMethod.sort(
+    (a, b) => b.temperature - a.temperature,
+  )[0]?.temperature
+
+  const low = processedWithArrMethod.sort(
+    (a, b) => a.temperature - b.temperature,
+  )[0]?.temperature
+
+  const average =
+    processedWithArrMethod.length > 0
+      ? processedWithArrMethod.reduce(
+          (acc, curr) => acc + curr.temperature,
+          0,
+        ) / processedWithArrMethod.length
+      : 0
+
+  const last = processedWithForOf.reverse()[0]?.temperature
+
+  if (date === null || city === null || processedWithArrMethod.length === 0) {
+    return null
+  }
+
+  return {
+    first,
+    last,
+    high,
+    low,
+    average,
+  }
 }
