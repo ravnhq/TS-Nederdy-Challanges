6b26fec335ef24e4ab9be6a5a86c5409c455f4ce
jainor <jainorcardenas@gmail.com>
2021-10-29 10:35:47 -0500

    feat: solve TS nerdery challenge

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,113 @@ interface TemperatureSummary {
   average: number
 }
 
+// DynamicTemperatureSummary adds new records and update values, to recalculate average we just need sum and number of records, for oher values we need current temperature value
+interface DynamicTemperatureSummary extends TemperatureSummary {
+  sum: number
+  nRecords: number
+}
+
+// MapEntries saves values of type V using a key of type string
+interface MapEntries<V> {
+  [key: string]: V
+}
+
+interface Modifier<K, V> {
+  getItem(key: K): V | undefined
+  addItem(key: K, value: V): V
+  updateItem(key: K, value: V, callback: CallbackUpdate<V>): V
+}
+
+// CallbackUpdate returns a value of type T that results from updating a previous value and a current value
+interface CallbackUpdate<T> {
+  (previous: T | undefined, current: any): T
+}
+
+interface UnsafeMapI<K extends string, V> extends Modifier<K, V> {
+  keys: MapEntries<V>
+}
+
+/**
+ * UnsafeMap stores values of type V given a key that extends from string.
+ * TODO: Make safer the UnsafeMap class
+ */
+class UnsafeMap<K extends string, V> implements UnsafeMapI<K, V> {
+  keys: MapEntries<V> = {}
+
+  public getItem(key: K): V {
+    return this.keys[key.toString()]
+  }
+  public addItem(key: K, value: V): V {
+    return (this.keys[key.toString()] = value)
+  }
+  public updateItem(key: K, value: any, callback: CallbackUpdate<V>): V {
+    return (this.keys[key.toString()] = callback(this.keys[key], value))
+  }
+}
+
+type cityName = string
+type dateStr = string
+const dataSummary = new UnsafeMap<
+  cityName,
+  UnsafeMap<dateStr, DynamicTemperatureSummary>
+>()
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  /**
+   * updateSummary initialize the variables of DynamicTemperatureSummary when the if condition is true,
+   * otherwise we return the value that results from updating item1 with value.
+   */
+  const updateSummary: CallbackUpdate<DynamicTemperatureSummary> = (
+    item1,
+    value: number,
+  ) => {
+    if (item1 == undefined) {
+      return {
+        first: value,
+        last: value,
+        high: value,
+        low: value,
+        average: value,
+        sum: value,
+        nRecords: 1,
+      }
+    }
+    return {
+      first: item1.first,
+      last: value,
+      high: Math.max(item1.high, value),
+      low: Math.min(item1.low, value),
+      average: (item1.sum + value) / (item1.nRecords + 1),
+      sum: item1.sum + value,
+      nRecords: item1.nRecords + 1,
+    }
+  }
+
+  readings.forEach((item): void => {
+    if (dataSummary.getItem(item.city) === undefined) {
+      dataSummary.addItem(
+        item.city,
+        new UnsafeMap<dateStr, DynamicTemperatureSummary>(),
+      )
+    }
+    dataSummary
+      .getItem(item.city)
+      .updateItem(item.time.toString(), item.temperature, updateSummary)
+  })
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  if (
+    dataSummary.getItem(city) == undefined ||
+    dataSummary.getItem(city).getItem(date.toString()) == undefined
+  ) {
+    return null
+  }
+
+  return dataSummary
+    .getItem(city)
+    .getItem(date.toString()) as TemperatureSummary
 }
