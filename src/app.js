ccba299e05a544598c02157a2953cb3f31be1762
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-04-20 15:49:05 -0600

    chore: add draft solution for challenge

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-exports.getTemperatureSummary = exports.processReadings = void 0;
+exports.getTemperatureSummary = void 0;
 function groupBy(objectArray, property) {
     return objectArray.reduce(function (acc, obj) {
         var key = obj[property];
@@ -12,28 +12,63 @@ function groupBy(objectArray, property) {
     }, {});
 }
 var readingsByCityAndDate;
-var processedReadingsStorage;
-function storeByCityAndDate(example) {
-    readingsByCityAndDate = groupBy(example, 'city');
+function storeByCityAndDate(readings) {
+    readingsByCityAndDate = groupBy(readings, 'city');
     for (var city in readingsByCityAndDate) {
         var element = readingsByCityAndDate[city];
         readingsByCityAndDate[city] = groupBy(element, 'time');
     }
+    return readingsByCityAndDate;
 }
 function processDailyReading(readingsByCityAndDate) {
-    for (var city in readingsByCityAndDate) {
-        var cityReadings = readingsByCityAndDate[city];
-        for (var date in cityReadings) {
-            var dailyReading = cityReadings[date];
-            // TODO: process dailyReading as per TemperatureSummary
-        }
-    }
+    var currentCity = '';
+    var currentDate = '';
+    Object.entries(readingsByCityAndDate).forEach(function (_a) {
+        var city = _a[0], dates = _a[1];
+        currentCity = city;
+        var readings = Object.values(dates);
+        readings.forEach(function (reading) {
+            currentDate = reading[0].time;
+            var temperatureSummary = {
+                first: 0,
+                last: 0,
+                high: 0,
+                low: 0,
+                average: 0
+            };
+            Object.keys(temperatureSummary).forEach(function (value) { return (temperatureSummary[value] = reading[0].temperature); });
+            if (reading.length > 2) {
+                temperatureSummary.last = reading[reading.length - 1].temperature;
+                var total = 0;
+                for (var index = 0; index < reading.length; index++) {
+                    var element = reading[index];
+                    total += element.temperature;
+                    if (element.temperature > temperatureSummary.high) {
+                        temperatureSummary.high = element.temperature;
+                    }
+                    if (element.temperature < temperatureSummary.low) {
+                        temperatureSummary.low = element.temperature;
+                    }
+                    temperatureSummary.average = total / reading.length;
+                }
+            }
+            readingsByCityAndDate[currentCity][currentDate] = temperatureSummary;
+        });
+    });
 }
 function processReadings(readings) {
-    storeByCityAndDate(example);
+    storeByCityAndDate(readings);
     processDailyReading(readingsByCityAndDate);
 }
-exports.processReadings = processReadings;
 processReadings(readings);
-function getTemperatureSummary(date, city) { }
+function getTemperatureSummary(date, city) {
+    var formattedDate = date.toString();
+    if (readingsByCityAndDate.hasOwnProperty(city) &&
+        readingsByCityAndDate[city].hasOwnProperty(formattedDate)) {
+        return readingsByCityAndDate[city][formattedDate];
+    }
+    else {
+        return null;
+    }
+}
 exports.getTemperatureSummary = getTemperatureSummary;
--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ interface TemperatureSummary {
   average: number
 }
 
-function groupBy(objectArray: object[], property: string) {
+function groupBy(objectArray: [], property: string) {
   return objectArray.reduce(function (acc, obj) {
     const key = obj[property]
     if (!acc[key]) {
@@ -22,29 +22,59 @@ function groupBy(objectArray: object[], property: string) {
   }, {})
 }
 
-let readingsByCityAndDate
+let readingsByCityAndDate: {
+  [x: string]: { [x: string]: TemperatureReading[] }
+}
 
-function storeByCityAndDate(example) {
-  readingsByCityAndDate = groupBy(example, 'city')
+function storeByCityAndDate(readings: TemperatureReading[]) {
+  readingsByCityAndDate = groupBy(readings, 'city')
   for (const city in readingsByCityAndDate) {
     const element = readingsByCityAndDate[city]
     readingsByCityAndDate[city] = groupBy(element, 'time')
   }
+  return readingsByCityAndDate
 }
 
 function processDailyReading(readingsByCityAndDate) {
-  processedReadingsStorage = Object.create(readingsByCityAndDate)
-  for (const city in readingsByCityAndDate) {
-    const cityReadings = readingsByCityAndDate[city]
-    for (const date in cityReadings) {
-      const dailyReading = cityReadings[date]
-      // TODO: process dailyReading as per TemperatureSummary
-    }
-  }
+  let currentCity = ''
+  let currentDate = ''
+  Object.entries(readingsByCityAndDate).forEach(([city, dates]) => {
+    currentCity = city
+    const readings = Object.values(dates)
+    readings.forEach((reading) => {
+      currentDate = reading[0].time
+      const temperatureSummary = {
+        first: 0,
+        last: 0,
+        high: 0,
+        low: 0,
+        average: 0,
+      }
+      Object.keys(temperatureSummary).forEach(
+        (value) => (temperatureSummary[value] = reading[0].temperature),
+      )
+      if (reading.length > 2) {
+        temperatureSummary.last = reading[reading.length - 1].temperature
+        let total = 0
+        for (let index = 0; index < reading.length; index++) {
+          const element = reading[index]
+          total += element.temperature
+          if (element.temperature > temperatureSummary.high) {
+            temperatureSummary.high = element.temperature
+          }
+          if (element.temperature < temperatureSummary.low) {
+            temperatureSummary.low = element.temperature
+          }
+          temperatureSummary.average = total / reading.length
+        }
+      }
+      readingsByCityAndDate[currentCity][currentDate] = temperatureSummary
+    })
+  })
 }
 
-export function processReadings(readings: TemperatureReading[]): void {
-  storeByCityAndDate(example)
+function processReadings(readings: TemperatureReading[]) {
+  storeByCityAndDate(readings)
   processDailyReading(readingsByCityAndDate)
 }
 
@@ -53,4 +83,14 @@ processReadings(readings)
 export function getTemperatureSummary(
   date: Date,
   city: string,
-): TemperatureSummary | null {}
+): TemperatureSummary | null {
+  const formattedDate = date.toString()
+  if (
+    readingsByCityAndDate.hasOwnProperty(city) &&
+    readingsByCityAndDate[city].hasOwnProperty(formattedDate)
+  ) {
+    return readingsByCityAndDate[city][formattedDate]
+  } else {
+    return null
+  }
+}
