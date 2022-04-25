f85a2132b388684fc588b0e4c2f42573d6836d81
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-04-25 13:35:39 -0600

    chore: finish challenge with alternate solution and save draft solution

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,62 @@
-import { TemperatureReading, TemperatureSummary } from './utils/interfaces'
-import { groupByCity, groupByDate } from './utils/utility-functions'
+import { TemperatureReading, TemperatureSummary } from './interfaces'
 
-let readingsByCityAndDate: {
-  [x: string]: { [x: string]: TemperatureSummary }
+let userReading: TemperatureSummary[]
+let readingsCopy: TemperatureReading[]
+
+export function processReadings(readings: TemperatureReading[]) {
+  readingsCopy = readings.slice()
 }
 
-function storeByCityAndDate(readings: TemperatureReading[]) {
-  readingsByCityAndDate = groupByCity(readings)
-  for (const city in readingsByCityAndDate) {
-    const date = readingsByCityAndDate[city]
-    readingsByCityAndDate[city] = groupByDate(date)
-  }
-}
-
-function processDailyReading() {
-  // FIX: iterating over object and modifying its properties at the same time
-  // Suggestion: create a deep copy of object to use as storage instead
-  // FIX: optimize processing algorithm
-  let currentCity = ''
-  let currentDate = ''
-  Object.entries(readingsByCityAndDate).forEach(([city, dates]) => {
-    currentCity = city
-    const readings = Object.values(dates)
-    readings.forEach((reading: TemperatureReading[]) => {
-      currentDate = reading[0].time
-      const temperatureSummary: TemperatureSummary = {
-        first: 0,
-        last: 0,
-        high: 0,
-        low: 0,
-        average: 0,
-      }
-      Object.keys(temperatureSummary).forEach(
-        (value) =>
-          (temperatureSummary[value as keyof typeof temperatureSummary] =
-            reading[0].temperature),
-      )
-      if (reading.length > 1) {
-        temperatureSummary.last = reading[reading.length - 1].temperature
-        let total = 0
-        for (let index = 0; index < reading.length; index++) {
-          const element = reading[index]
-          total += element.temperature
-          if (element.temperature > temperatureSummary.high) {
-            temperatureSummary.high = element.temperature
-          }
-          if (element.temperature < temperatureSummary.low) {
-            temperatureSummary.low = element.temperature
-          }
-          temperatureSummary.average = total / reading.length
-        }
-      }
-      readingsByCityAndDate[currentCity][currentDate] = temperatureSummary
-    })
-  })
-}
-
-export function processReadings(readings: TemperatureReading[]): void {
-  storeByCityAndDate(readings)
-  processDailyReading()
-}
-
-// processReadings(readings)
-
-export function getTemperatureSummary(
-  date: Date,
-  city: string,
-): TemperatureSummary | null {
+export function getTemperatureSummary(date: Date, city: string) {
+  userReading = []
   const formattedDate = date.toString()
-  if (
-    readingsByCityAndDate.hasOwnProperty(city) &&
-    readingsByCityAndDate[city].hasOwnProperty(formattedDate)
-  ) {
-    return readingsByCityAndDate[city][formattedDate]
+  const cityArray = filterBy(readingsCopy, 'city', city)
+  const dateArray = filterBy(cityArray, 'time', formattedDate)
+  processDailyReading(dateArray)
+  if (userReading.length) {
+    return userReading[0]
   } else {
     return null
   }
 }
 
-// console.log(getTemperatureSummary(new Date('1/2/2021'), 'Utah'))
+function filterBy(array: TemperatureReading[], property: any, userParam: any) {
+  return array.filter(
+    (element) =>
+      element[property as keyof TemperatureReading].toString() === userParam,
+  )
+}
+
+function processDailyReading(array: TemperatureReading[]) {
+  if (!array.length) {
+    return
+  }
+  const temperatureSummary = {
+    first: 0,
+    last: 0,
+    high: 0,
+    low: 0,
+    average: 0,
+  }
+  Object.keys(temperatureSummary).forEach(
+    (value) =>
+      (temperatureSummary[value as keyof typeof temperatureSummary] =
+        array[0].temperature),
+  )
+  if (array.length > 1) {
+    temperatureSummary.last = array[array.length - 1].temperature
+    let total = 0
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index]
+      total += element.temperature
+      if (element.temperature > temperatureSummary.high) {
+        temperatureSummary.high = element.temperature
+      }
+      if (element.temperature < temperatureSummary.low) {
+        temperatureSummary.low = element.temperature
+      }
+      temperatureSummary.average = total / array.length
+    }
+  }
+  userReading.push(temperatureSummary)
+}
