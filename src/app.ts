6aa7d2bb4128dcc3c8c5e9015d4807a8efc59590
Brayan Vera <brayanvera@ravn.co>
2022-07-19 18:46:37 -0500

    progress on the challenge

--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,39 @@ interface TemperatureSummary {
   low: number
   average: number
 }
+const temperatureReadings: TemperatureReading[] = []
 
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  temperatureReadings.push(...readings)
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  // verify if same city first
+  const specificReadings: TemperatureSummary = temperatureReadings.reduce(
+    // QUESTION: why when I add type TemperatureReading to temperatureReading on the line below, I get a: No overloads matches this call (even though temperatureReadings is an array of type TemperatureReading)
+    (acc: any, temperatureReading /*:TemperatureReading*/) => {
+      if (date !== temperatureReading.time) {
+        // console.log(`1 ${date} and ${city} got me in here`)
+        return
+      }
+      if (city !== temperatureReading.city) {
+        // console.log(`2 ${date} and ${city} got me in here`)
+        return
+      }
+
+      acc.first = temperatureReading.temperature
+      acc.last = temperatureReading.temperature
+      acc.high = temperatureReading.temperature
+      acc.low = temperatureReading.temperature
+      acc.average = temperatureReading.temperature
+      return
+    },
+    { first: null, last: null, high: null, low: null, average: null },
+  )
+  console.log(specificReadings)
+
+  return specificReadings
 }
