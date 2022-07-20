55898ce744abda57501bd7d8a5a94aeff4dd887f
Brayan Vera <brayanvera@ravn.co>
2022-07-20 11:38:12 -0500

    refactored and ended challenge

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ interface TemperatureSummary {
   low: number
   average: number
 }
-const temperatureReadings: TemperatureReading[] = []
+const temperatureReadings: Array<TemperatureReading> = []
 
 export function processReadings(readings: TemperatureReading[]): void {
   temperatureReadings.push(...readings)
@@ -45,7 +45,6 @@ export function getTemperatureSummary(
       }
       if (index === filteredReadings.length - 1) {
         acc.last = temperatureReading.temperature
-        console.log(temperatureArray.length)
         acc.average =
           temperatureArray.reduce((acc, elm) => acc + elm, 0) /
           temperatureArray.length
@@ -66,8 +65,8 @@ export function getTemperatureSummary(
       average: ZERO_WAITING_ASSIGNATION,
     },
   )
-
   return temperatureSummary
+
   function isSameDateAndCity(
     temperatureReading: TemperatureReading,
     date: Date,
@@ -78,64 +77,3 @@ export function getTemperatureSummary(
     return true
   }
 }
-/* stuff to be deleted*/
-const example = [
-  {
-    time: new Date('1/3/2021'),
-    temperature: 8,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 10,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 9,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 12,
-    city: 'Utah',
-  },
-  {
-    time: new Date('1/2/2021'),
-    temperature: 11,
-    city: 'Utah',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 15,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 10,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 11,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/12/2021'),
-    temperature: 9,
-    city: 'New York',
-  },
-  {
-    time: new Date('3/13/2021'),
-    temperature: 16,
-    city: 'New York',
-  },
-]
-processReadings(example)
-const result = getTemperatureSummary(new Date('3/12/2021'), 'Texas')
-// const result = getTemperatureSummary(new Date('1/2/2021'), 'Utah')
-// const result = getTemperatureSummary(new Date('3/12/2021'), 'New York')
-console.log('result')
-console.log(result)
-
-/** */
