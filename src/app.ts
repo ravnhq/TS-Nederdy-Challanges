e749b14abd9602192fe0c12dd4839c43fd24d95e
Samuel Mundo <samuelmundo@ravn.co>
2023-04-13 10:53:10 -0600

    TS Challenge

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,53 @@ interface TemperatureSummary {
   average: number
 }
 
+let readingsArray: TemperatureReading[] = []
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  readingsArray = readings
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  //Function to filter the global array so it only contains readings from the requested city and date
+  const isFromDateAndCity = (read: TemperatureReading): boolean => {
+    return read.time.getDate() === date.getDate() && read.city === city
+  }
+
+  //Function to find the highest temp from a readings array
+  const findHighestTemp = (reads: TemperatureReading[]): TemperatureReading => {
+    return reads.reduce(function (prev, current) {
+      return prev.temperature > current.temperature ? prev : current
+    })
+  }
+
+  //Function to find the lowest temp from a readings array
+  const findLowestTemp = (reads: TemperatureReading[]): TemperatureReading => {
+    return reads.reduce(function (prev, current) {
+      return prev.temperature < current.temperature ? prev : current
+    })
+  }
+
+  //Function to calculate the average temp from a readings array
+  const findAvgTemp = (reads: TemperatureReading[]): number => {
+    const avg = reads.reduce((accu, curr) => accu + curr.temperature, 0)
+    return avg / reads.length
+  }
+
+  const filteredCity = readingsArray.filter(isFromDateAndCity)
+
+  //If the filtered cities array is empty this means the requested city does not exist, therefore we return null.
+  if (filteredCity.length === 0) return null
+
+  const SummaryData: TemperatureSummary = {
+    first: filteredCity[0].temperature,
+    last: filteredCity[filteredCity.length - 1].temperature,
+    high: findHighestTemp(filteredCity).temperature,
+    low: findLowestTemp(filteredCity).temperature,
+    average: findAvgTemp(filteredCity),
+  }
+
+  return SummaryData
 }
