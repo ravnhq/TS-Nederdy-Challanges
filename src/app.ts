afd74fa4c5d47dc85d35d97ded924b16d947a3a2
Eduardo Rodriguez <eduardo_sdacr@hotmail.com>
2023-04-26 11:51:19 -0500

    feat: complete functions code in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,42 @@ interface TemperatureSummary {
   average: number
 }
 
+let temperatureData: TemperatureReading[]
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  temperatureData = readings
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const foundCity = temperatureData.find(
+    (item: TemperatureReading) => item.city === city,
+  )
+  if (foundCity === undefined) return null
+
+  const cityTemperatureReadings: TemperatureReading[] = temperatureData.filter(
+    (item: TemperatureReading) =>
+      item.city === city && item.time.getDay() === date.getDay(),
+  )
+  const cityTemperatures: number[] = cityTemperatureReadings.map(
+    (item: TemperatureReading) => item.temperature,
+  )
+
+  const firstTemp: number = cityTemperatures[0]
+  const lastTemp: number = cityTemperatures.slice(-1)[0]
+  const highTemp: number = Math.max(...cityTemperatures)
+  const lowTemp: number = Math.min(...cityTemperatures)
+  const averageTemp: number =
+    cityTemperatures.reduce(
+      (total: number, temperature: number) => total + temperature,
+    ) / cityTemperatures.length
+
+  return {
+    first: firstTemp,
+    last: lastTemp,
+    high: highTemp,
+    low: lowTemp,
+    average: averageTemp,
+  }
 }
